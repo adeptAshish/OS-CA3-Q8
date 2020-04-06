bc99b45308c750014c18f993452e0c1053282fe9
Ashish Rawat <57556638+adeptAshish@users.noreply.github.com>
2020-04-06 23:36:18 +0530

    Add files via upload

--- 11810804-OS-CA3.c
+++ 11810804-OS-CA3.c
@@ -124,51 +124,6 @@ void faculty_Queue(int no_of_process) {
 }
  
  
-void student_Queue(int no_of_process) {
- 
-    int count, arrival_Time, burst_Time, quantum_time;
-    struct process_Struct student_Process[no_of_process];
- 
-    for(count = 0; count < no_of_process; count++) {
-        printf("Enter the details of Process[%d]", count+1);
-        puts("");
-        printf("Process Name : ");
-        scanf("%s", student_Process[count].process_name);
- 
-        printf("Arrival Time : ");
-        scanf("%d", &student_Process[count].arrival_time);
- 
-        printf("Burst Time : ");
-        scanf("%d", &student_Process[count].burst_time);
-    }
-    printf("Now, enter the quantum time for STUDENT queue : ");
-    scanf("%d", &quantum_time);
- 
- 
-    // sorting the processes by their ARRIVAL time.
-    // if the ARRIVAL time is same then scheduling is based on FCFS.
-    for(count = 0; count < no_of_process; count++) {
-        for(int x = count +1; x < count; x++){
-            if(student_Process[count].arrival_time > student_Process[x].arrival_time) {
-                temp_Struct = student_Process[count];
-                student_Process[count] = student_Process[x];
-                student_Process[x] = temp_Struct;
-            }
-        }
-    }
- 
-    // initialy all the burst time is remaining and completion of process is zero.
-    for(count = 0; count < no_of_process; count++) {
-        student_Process[count].remaining = student_Process[count].burst_time;
-        student_Process[count].completion_time = 0;
-    }
- 
-    int total_time, queue, round_robin[20];
-    total_time = 0;
-    queue = 0;
-    round_robin[queue] = 0;
-}
- 
  
 int main(int argc, char const *argv[]) {
     int select_queue, no_of_process;
